3e945e9cb2cab59fcd563221c1ddc2be8af6ad19
toadlover <toadloverarig@gmail.com>
2025-12-22 17:05:15 -0600

    adding max job throttle

--- shapedb/nnsearch_controller.py
+++ shapedb/nnsearch_controller.py
@@ -13,8 +13,8 @@ if len(sys.argv) > 2:
 	working_location = sys.argv[2]
 
 #iterate over every chunk, and call the search script on each subchunk within the chunk
-#for i in range(0,53085):
-for i in range(0,1500):
+for i in range(0,53085):
+#for i in range(0,1500):
 	#build the chunk string
 	chunk_str = str(i)
 
@@ -28,3 +28,19 @@ for i in range(0,1500):
 	#submit a bsub job that runs the nnsearch python script
 	print("bsub -q long -W 8:00 -u \"\" \"python /pi/summer.thyme-umw/enamine-REAL-2.6billion/umass_chan_REAL-M_platform/shapedb/run_nnsearch_hpc.py " + chunk_str + " " +  target_molecule_file + " " + working_location + " \"")
 	os.system("bsub -q long -W 8:00 -u \"\" \"python /pi/summer.thyme-umw/enamine-REAL-2.6billion/umass_chan_REAL-M_platform/shapedb/run_nnsearch_hpc.py " + chunk_str + " " +  target_molecule_file + " " + working_location + " \"")
+
+	#adding 500 job throttle
+	#bsub job throttle to make sure we do not exceed our local limit
+	#write the length of the bjobs queue to this current location
+	os.system("bjobs | wc -l > bjobs_length.txt")
+	job_count = 0
+	with open("bjobs_length.txt") as f:
+		job_count = int(f.read().strip())
+	while job_count > 500:
+		#sleep for 1 second to not overburden the system
+		os.system("sleep 1")
+		os.system("bjobs | wc -l > bjobs_length.txt")
+		with open("bjobs_length.txt") as f:
+			job_count = int(f.read().strip())
+	#remove the length file to avoid clutter
+	os.system("rm bjobs_length.txt")
