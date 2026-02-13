87096da00dba06075d08c07e9e4f879019af155b
Ginsparg <abgvg9@uab.edu>
2026-02-13 13:51:27 -0500

    updating code to also keep any residues with a recorded shift, based on heavy atom center of mass comparisons with reference

--- tidying/shrink_placement_pdbs_to_placement_and_surrounding_residues.py
+++ tidying/shrink_placement_pdbs_to_placement_and_surrounding_residues.py
@@ -9,10 +9,13 @@ imports os,sys
 
 working_location = os.getcwd()
 
-#arguments (optional path to go to operate in)
-if len(sys.argv) == 2:
+#arguments (pre-docked PDB for residue shift reference; optional path to go to operate in)
+#reference pdb (this assumes that the placement system and the reference are aligned, which they should be if the placement was derived from a rosetta run with this placement with no other operations performed on it)
+reference_pdb = sys.argv[1]
+
+if len(sys.argv) == 3:
 	#get the different location
-	working_location = sys.argv[1]
+	working_location = sys.argv[2]
 
 #move to the working location
 os.chdir(working_location)
@@ -24,6 +27,82 @@ os.system("tar -xzf placements.tar.gz")
 os.chdir("placements")
 
 placements_location = os.getcwd()
+
+#read the reference pdb and get all residues for center of mass of heavy atoms (no hydrogens)
+#doing center of mass shift because atom index names may not align between pdb given to rosetta and rosetta placement pdb
+ref_file = open(reference_pdb,"r")
+
+#dictionary to hold the reference pdb residue centers of mass
+#key is a tuple of the reference residue index (may not be the same as rosetta output) and residue 3 letter code
+ref_res_com = {}
+
+#have a working list of atom coordinates that builds as we find residue atoms
+ref_res_atoms = []
+cur_pair = ["",""]
+
+for line in read_file.readlines():
+	#if it is an ATOM line, we have something to work with
+	if line.startswith("ATOM"):
+		#if the atom is a hydrogen, skip (last character in line is H)
+		if line.strip().endswith("H"):
+			continue
+
+		#get atom xyz
+		atom_xyz = [float(line.split()[6]),float(line.split()[7]),float(line.split()[8])]
+
+		#get atom residue index and code
+		atom_res_index = line.split()[5]
+		atom_res_code = line.split()[3]
+
+		#if starting with the first atom, change the current residue to the residue we just hit
+		if cur_pair[0] == "" and cur_pair[1] == "":
+			cur_pair[0] = atom_res_index
+			cur_pair[1] = atom_res_code
+
+		#if the current atom is in the current residue, add it to the reference residue atoms
+		if cur_res == atom_res_index:
+			ref_res_atoms.append(atom_xyz)
+		#otherwise, if there is a mismatch, we hit the end of the residue and should derive the center of mass
+		else:
+			#derive the sum of each dimension
+			x_sum = 0
+			y_sum = 0
+			z_sum = 0
+			for atom in ref_res_atoms:
+				x_sum = x_sum + atom[0]
+				y_sum = y_sum + atom[1]
+				z_sum = z_sum + atom[2]
+
+			#get the com coordinate by averaging the sum by number of atoms
+			#assign to the dictionary
+			ref_res_com[cur_pair] = [x_sum / len(ref_res_atoms), y_sum / len(ref_res_atoms), z_sum / len(ref_res_atoms)]
+
+			#now, set new cur_pair values since we are moving on and reset the ref_res_atoms list for the new residue
+			cur_pair[0] = atom_res_index
+			cur_pair[1] = atom_res_code
+
+			ref_res_atoms = []
+			ref_res_atoms.append(atom_xyz)
+
+	#at the end when we hit the TER line to indicate end of protein/chain, get the com for the last residue
+	if line.startswith("TER") and len(ref_res_atoms) > 0 and cur_pair[0] != "" and cur_pair[1] != "":
+		#derive the sum of each dimension
+		x_sum = 0
+		y_sum = 0
+		z_sum = 0
+		for atom in ref_res_atoms:
+			x_sum = x_sum + atom[0]
+			y_sum = y_sum + atom[1]
+			z_sum = z_sum + atom[2]
+
+		#get the com coordinate by averaging the sum by number of atoms
+		#assign to the dictionary
+		ref_res_com[cur_pair] = [x_sum / len(ref_res_atoms), y_sum / len(ref_res_atoms), z_sum / len(ref_res_atoms)]
+
+		ref_res_atoms = []		
+
+
+
 
 #iterate over the directory and look for placement pdb files
 for r,d,f in os.walk(placements_location):
@@ -79,7 +158,54 @@ for r,d,f in os.walk(placements_location):
 			#list of residues to keep by index
 			res_to_keep = []
 
+			
 			for res in prot_res.keys():
+				#keep any residues with any shift so we retain that data
+				#determine the residue heavy atom center of mass
+				res_com = [0,0,0]
+				nheavy_atoms = 0
+				#get residue 3 letter code
+				residue_code = ""
+				for res_atom in prot_res[res]:
+					#skip hydrogens
+					if res_atom.strip().endswith("H"):
+						continue
+
+					#extract the residue atom coordinates to add to the res com
+					res_com[0] = res_com[0] + float(res_atom.split()[6])
+					res_com[1] = res_com[1] + float(res_atom.split()[7])
+					res_com[2] = res_com[2] + float(res_atom.split()[8])
+					nheavy_atoms = nheavy_atoms + 1
+
+					if residue_code != "":
+						residue_code = res_atom.split()[3]
+
+				#divide the residue com sums by the number of atoms to get a usable average
+				res_com[0] = res_com[0] / nheavy_atoms
+
+				shortest_distance = 100
+
+				#run through each residue in the reference and determine which residue is the closest by com and then what the distance is (if greater than 0, we should keep; may have to test and see if there are rounding issues)
+				for ref_res in ref_res_com.keys():
+					#if the residue code matches the reference residue, consider for comparison
+					if residue_code == ref_res[1]:
+						#get the distance between the reference and placement residue coms
+						distance = (((ref_res_com[ref_res][0] - res_com[0]) ** 2) + ((ref_res_com[ref_res][1] - res_com[1]) ** 2) + ((ref_res_com[ref_res][2] - res_com[2]) ** 2)) ** 0.5
+
+						#check if the distance is shorter than the shortest recorded distance, and if so, replace (no need to track the residue)
+						#if the distance is 0, go ahead and break since we don't need to check with any residues
+						if distance < shortest_distance:
+							shortest_distance = distance
+
+						if shortest_distance == 0:
+							break
+
+				#if the shortest distance is greater than 0 (meaning that the residue moved any amount), add it to residues to keep and continue, skipping proximity keep
+				if shortest_distance > 0:
+					res_to_keep.append(res)
+					continue
+
+				#look for residues within 5 angstroms to keep
 				for res_atom in prot_res[res]:
 					#extract the residue atom coordinates
 					x = float(res_atom.split()[6])
@@ -99,6 +225,7 @@ for r,d,f in os.walk(placements_location):
 					#if we added the residue to the res_to_keep, break here to move to the next residue
 					if res in res_to_keep:
 						break
+			
 
 			#finally, go back over the buffered file, and when ew get to ATOM lines, determine whether or not to keep the residue based on if the residue is in the res_to_keep
 			write_file = ("temp.pdb", "w")
