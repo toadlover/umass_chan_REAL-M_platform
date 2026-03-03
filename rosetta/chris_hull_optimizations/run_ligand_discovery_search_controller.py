2696de700ae610a2d75dd158045d0cec983ac1ae
Ginsparg <abgvg9@uab.edu>
2026-03-03 14:09:37 -0500

    attempting a fix

--- rosetta/chris_hull_optimizations/run_ligand_discovery_search_controller.py
+++ rosetta/chris_hull_optimizations/run_ligand_discovery_search_controller.py
@@ -141,7 +141,7 @@ bsub_cmd = (
     f"-R \"rusage[tmp={tmp_mb}]\" "
     f"-o logs/%J_%I.out "
     f"-e logs/%J_%I.err "
-    f"bash {wrapper} $(sed -n \"$LSB_JOBINDEX\"p " + starting_location + "/joblist.txt)"
+    f"bash {wrapper} $(sed -n \"\\$LSB_JOBINDEX\"p " + starting_location + "/joblist.txt)"
 )
 
 print("\nSubmitting LSF job array:")
